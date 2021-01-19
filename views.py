a24168f9200dc68e8d3052ffdf15901e8e64c9c1
Jan Staněk <jan@jstanek.cz>
2021-01-19 19:45:54 +0100

    views by kraj

--- views.py
+++ views.py
@@ -19,7 +19,7 @@ def index():
 def info_mesto(mesto):
     nactene_informace = app.session.query(OckovaciKapacity).from_statement(
         text(
-            "SELECT m.mesto, m.nazev, k.datum, k.pocet_mist, k.misto_id FROM ockovaci_misto m "
+            "SELECT m.mesto, m.kraj, m.nazev, k.datum, k.pocet_mist, k.misto_id FROM ockovaci_misto m "
             "JOIN kapacita k ON (m.misto_id=k.misto_id) "
             "WHERE m.mesto=:mesto_param "
             "ORDER BY k.datum, m.nazev"
@@ -29,11 +29,25 @@ def info_mesto(mesto):
     return render_template('mesto.html', data=nactene_informace, mesto=mesto, last_update=last_update())
 
 
+@my_view.route("/kraj/<kraj>")
+def info_kraj(kraj):
+    nactene_informace = app.session.query(OckovaciKapacity).from_statement(
+        text(
+            "SELECT m.mesto, m.kraj, m.nazev, k.datum, k.pocet_mist, k.misto_id FROM ockovaci_misto m "
+            "JOIN kapacita k ON (m.misto_id=k.misto_id) "
+            "WHERE m.kraj=:kraj_param "
+            "ORDER BY k.datum, m.mesto, m.nazev"
+        )
+    ).params(kraj_param=kraj).all()
+
+    return render_template('kraj.html', data=nactene_informace, kraj=kraj, last_update=last_update())
+
+
 @my_view.route("/misto/<misto>")
 def info_misto(misto):
     nactene_informace = app.session.query(OckovaciKapacity).from_statement(
         text(
-            "SELECT m.mesto, m.nazev, k.datum, k.pocet_mist, k.misto_id FROM ockovaci_misto m "
+            "SELECT m.mesto, m.kraj, m.nazev, k.datum, k.pocet_mist, k.misto_id FROM ockovaci_misto m "
             "JOIN kapacita k ON (m.misto_id=k.misto_id) "
             "WHERE m.misto_id=:misto_param "
             "ORDER BY k.datum"
@@ -41,7 +55,7 @@ def info_misto(misto):
     ).params(misto_param=misto).all()
 
     ockovani_info = app.session.query(OckovaciMisto).from_statement(text(
-        "SELECT m.misto_id, m.nazev, m.service_id, m.operation_id, m.place_id, m.mesto "
+        "SELECT m.misto_id, m.nazev, m.service_id, m.operation_id, m.place_id, m.mesto, m.kraj "
         "FROM public.ockovaci_misto m "
         "WHERE m.misto_id=:misto_param"
     )).params(misto_param=misto).first()
@@ -53,10 +67,10 @@ def info_misto(misto):
 def info():
     ockovani_info = app.session.query(OckovaciMisto.misto_id, OckovaciMisto.nazev, OckovaciMisto.service_id,
                                       OckovaciMisto.operation_id,
-                                      OckovaciMisto.place_id, OckovaciMisto.mesto,
+                                      OckovaciMisto.place_id, OckovaciMisto.mesto, OckovaciMisto.kraj,
                                       func.max(Kapacita.pocet_mist).label("pocet_mist")).outerjoin(Kapacita, Kapacita.misto_id == OckovaciMisto.misto_id).group_by(
         OckovaciMisto.misto_id, OckovaciMisto.nazev, OckovaciMisto.service_id, OckovaciMisto.operation_id,
-        OckovaciMisto.place_id, OckovaciMisto.mesto).all()
+        OckovaciMisto.place_id, OckovaciMisto.mesto, OckovaciMisto.kraj).all()
     return render_template('ockovani_info.html', ockovaci_mista=ockovani_info, last_update=last_update())
 
 
