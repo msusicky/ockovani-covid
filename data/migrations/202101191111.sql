c09a8676775cbf19f7a8a1ee9a17f5230add3e9d
Jan Staněk <jan@jstanek.cz>
2021-02-20 09:16:17 +0100

    invalid migrate removed

--- data/migrations/202101191111.sql
+++
@@ -1,4 +0,0 @@
--- !!!already part of db_create!!!
-
-alter table public.kapacita alter column datum_ziskani set default now();
-ALTER TABLE import_log ADD COLUMN status character varying COLLATE pg_catalog."default" NOT NULL DEFAULT 'N/A';
