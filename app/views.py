2f3ea4d90e7ad4d52c87f3f7b8c119f93634b3eb
Jan Staněk <jan@jstanek.cz>
2021-02-28 19:38:57 +0100

    map page, opendata page

--- app/views.py
+++ app/views.py
@@ -26,9 +26,9 @@ def info_okres(okres_nazev):
         .outerjoin(Okres, (OckovaciMisto.okres_id == Okres.id)) \
         .outerjoin(Kraj, (Okres.kraj_id == Kraj.id)) \
         .filter(Okres.nazev == okres_nazev) \
-        .filter(
-        VolnaMistaDen.import_id == last_update_import_id()).order_by(
-        VolnaMistaDen.datum, OckovaciMisto.nazev).all()
+        .filter(VolnaMistaDen.import_id == last_update_import_id())\
+        .order_by(VolnaMistaDen.datum, OckovaciMisto.nazev)\
+        .all()
 
     return render_template('okres.html', data=nactene_informace, okres=okres, last_update=last_update())
 
@@ -46,9 +46,9 @@ def info_kraj(kraj_name):
         .outerjoin(Okres, (OckovaciMisto.okres_id == Okres.id)) \
         .outerjoin(Kraj, (Okres.kraj_id == Kraj.id)) \
         .filter(Kraj.nazev == kraj_name) \
-        .filter(
-        VolnaMistaDen.import_id == last_update_import_id()).order_by(
-        VolnaMistaDen.datum, OckovaciMisto.okres, OckovaciMisto.nazev).all()
+        .filter(VolnaMistaDen.import_id == last_update_import_id())\
+        .order_by(VolnaMistaDen.datum, OckovaciMisto.okres, OckovaciMisto.nazev)\
+        .all()
 
     return render_template('kraj.html', data=nactene_informace, kraj=kraj, last_update=last_update())
 
@@ -69,8 +69,9 @@ def info_misto(misto_id):
         .outerjoin(Okres, (OckovaciMisto.okres_id == Okres.id)) \
         .outerjoin(Kraj, (Okres.kraj_id == Kraj.id)) \
         .filter(OckovaciMisto.id == misto.id) \
-        .filter(VolnaMistaDen.import_id == last_update_import_id()).order_by(
-        VolnaMistaDen.datum).all()
+        .filter(VolnaMistaDen.import_id == last_update_import_id())\
+        .order_by(VolnaMistaDen.datum)\
+        .all()
 
     ampule_info = db.session.query("vyrobce", "operace", "sum").from_statement(text(
         """
@@ -171,7 +172,7 @@ def _compute_vaccination_total(total):
 
 
 @bp.route("/mista")
-def info():
+def mista():
     ockovani_info = db.session.query(OckovaciMisto.id, OckovaciMisto.nazev, OckovaciMisto.adresa,
                                      OckovaciMisto.latitude, OckovaciMisto.longitude,
                                      OckovaciMisto.minimalni_kapacita,
@@ -192,6 +193,17 @@ def info():
         .order_by(Kraj.nazev, Okres.nazev, OckovaciMisto.nazev).all()
 
     return render_template('mista.html', ockovaci_mista=ockovani_info, last_update=last_update())
+
+
+@bp.route("/mapa")
+def mapa():
+    db.session.query(OckovaciMisto).all()
+    return render_template('mapa.html', ockovaci_mista=mista, last_update=last_update())
+
+
+@bp.route("/opendata")
+def opendata():
+    return render_template('opendata.html', last_update=last_update())
 
 
 def last_update():
