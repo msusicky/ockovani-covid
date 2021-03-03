4faa8b9460428a075b35dd1c0e677f4a28149536
Marek Susicky <marek@susicky.net>
2021-03-03 21:24:42 +0100

    Pages moved

--- tools/create_static_pages.sh
+++ tools/create_static_pages.sh
@@ -1,10 +1,10 @@
-wget -r http://127.0.0.1:5000/ockovani-covid/
+wget -r http://127.0.0.1:5000/
 find ./127.0.0.1:5000/ -type f -print0 | xargs -0 -I{} mv "{}" "{}".html
-mv ./127.0.0.1:5000/ockovani-covid/index.html.html ./127.0.0.1:5000/ockovani-covid/index.html
+mv ./127.0.0.1:5000/index.html.html ./127.0.0.1:5000/index.html
 
-cp -r 127.0.0.1:5000/* /home/ockovani/web/msusicky.github.io/
+cp -r 127.0.0.1:5000/* /home/ockovani/web/ockovani-covid-pages/
 rm -r 127.0.0.1:5000
-cd /home/ockovani/web/msusicky.github.io/ockovani-covid
+cd /home/ockovani/web/ockovani-covid-pages/
 git add *
 git commit -m "Update"
 git push origin master
