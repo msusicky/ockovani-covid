dabebd39ce297e849ff8d8113e0ae7e66f8e13f8
Jan Staněk <jan@jstanek.cz>
2021-03-04 23:08:08 +0100

    old fetchers removed

--- app/commands.py
+++ app/commands.py
@@ -1,14 +1,6 @@
 from app import app
 from app.opendata_fetcher import OpenDataFetcher
-from app.reservatic_fetcher import ReservaticFetcher
-from app.sheet_fetcher import SheetFetcher
 
-
-@app.cli.command('fetch-reservatic')
-def fetch_reservatic_command():
-    """Fetch free capacities from Reservatic."""
-    fetcher = ReservaticFetcher()
-    fetcher.fetch_free_capacities()
 
 @app.cli.command('fetch-opendata')
 def fetch_opendata_command():
@@ -22,21 +14,3 @@ def fetch_opendata_command():
     else:
         app.logger.error("Fetching opendata failed.")
         exit(1)
-
-@app.cli.command('fetch-sheet')
-def fetch_sheet_command():
-    """Fetch sheet from Google Sheets."""
-    fetcher = SheetFetcher()
-    fetcher.fetch_centers()
-
-@app.cli.command('fetch-all')
-def fetch_all_command():
-    """Execute all fetchers."""
-    opendata_fetcher = OpenDataFetcher()
-    opendata_fetcher.fetch_all()
-
-    sheet_fetcher = SheetFetcher()
-    sheet_fetcher.fetch_centers()
-
-    reservatic_fetcher = ReservaticFetcher()
-    reservatic_fetcher.fetch_free_capacities()
