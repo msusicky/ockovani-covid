e67ae9b7f010f1510efa9ca84f0e84a0f4a1c5d4
Jan Staněk <jan@jstanek.cz>
2021-03-06 16:15:13 +0100

    pipeline

--- config.sample.py
+++ config.sample.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ockovani:ockovani@ockovani:5432/ockovani'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ockovani:ockovani@localhost:5432/ockovani'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
