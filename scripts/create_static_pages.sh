80ddfe230f515266b7bbcade7e986f3d5cef7f72
Jan Staněk <jan@jstanek.cz>
2021-03-06 17:36:25 +0100

    pipeline

--- scripts/create_static_pages.sh
+++ scripts/create_static_pages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wget -r http://127.0.0.1:${1}/
-find ./127.0.0.1:${1}/ -type f -print0 | xargs -0 -I{} mv "{}" "{}".html
-mv ./127.0.0.1:${1}/index.html.html ./127.0.0.1:${1}/index.html
+wget -r http://127.0.0.1:5000/
+find ./127.0.0.1:5000/ -type f -print0 | xargs -0 -I{} mv "{}" "{}".html
+mv ./127.0.0.1:5000/index.html.html ./127.0.0.1:5000/index.html
 
-cp -r 127.0.0.1:${1}/* /home/ockovani/prd/web/ockovani-covid-pages/
-rm -r 127.0.0.1:${1}
+cp -r 127.0.0.1:5000/* /home/ockovani/prd/web/ockovani-covid-pages/
+rm -r 127.0.0.1:5000
 cd /home/ockovani/prd/web/ockovani-covid-pages
 git add *
 git commit -m "Update"
