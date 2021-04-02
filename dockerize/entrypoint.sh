5b852327f6d88b00788f0a1bfa5de0c2f3b330f9
Jan Staněk <jan@jstanek.cz>
2021-04-02 21:59:42 +0200

    command fix

--- dockerize/entrypoint.sh
+++ dockerize/entrypoint.sh
@@ -5,7 +5,7 @@ venv3/bin/flask db upgrade
 # fetch data if env var is set
 if [ "$FETCH_DATA" = "true" ]; then
     echo "Fetching fresh data"
-    venv3/bin/flask fetch-data
+    venv3/bin/flask fetch-data all
     venv3/bin/flask compute-metrics all
 fi
 venv3/bin/flask run --host 0.0.0.0 --port 5000
