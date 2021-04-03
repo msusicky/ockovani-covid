61de85000125dcb89e8d205519144d6ffaf774f7
Jan Staněk <jan@jstanek.cz>
2021-04-03 13:45:35 +0200

    group by age category

--- app/fetcher/deaths_fetcher.py
+++ app/fetcher/deaths_fetcher.py
@@ -18,7 +18,20 @@ class DeathsFetcher(Fetcher):
     def fetch(self, import_id: int) -> None:
         df = pd.read_csv(self._url)
 
-        df = df[['datum', 'vek', 'kraj_nuts_kod']]
+        vekova_skupina = pd.read_sql_query('select vekova_skupina, min_vek, max_vek from populace_kategorie', db.engine)
+        vekova_skupina['join'] = 0
+
+        vek = pd.Series(range(0, 151), name='vek').to_frame()
+        vek['join'] = 0
+
+        merged = pd.merge(vek, vekova_skupina)
+        merged = merged.where((merged['vek'] >= merged['min_vek']) & (merged['vek'] <= merged['max_vek'])).dropna()
+
+        df = pd.merge(df, merged, how='left')
+
+        df = df[['datum', 'vekova_skupina', 'kraj_nuts_kod']]
+
+        df['kraj_nuts_kod'] = df['kraj_nuts_kod'].fillna('-')
 
         df = df.groupby(df.columns.tolist(), dropna=False).size().reset_index(name='pocet')
 
