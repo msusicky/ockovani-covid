4227cae4009f189d6be2b3dee16aac34475e0a73
Jan Staněk <jan@jstanek.cz>
2021-04-12 21:12:54 +0200

    multiple towns

--- app/queries.py
+++ app/queries.py
@@ -514,13 +514,8 @@ def count_vaccinated_doctors(kraj_id=None):
         select nazev_cely as nazev, zarizeni_kod, obec, sum(pocet) as sum_1, 
             sum(case when ol.datum+'7 days'::interval>='{}' then pocet else 0 end)  as sum_2
         from ockovani_lide ol join (
-            SELECT max(nazev_cely) nazev_cely, nrpzs_kod, max(obec) obec 
+            SELECT min(nazev_cely) nazev_cely, nrpzs_kod, string_agg(distinct obec, ' / ') obec 
             from zdravotnicke_stredisko zs 
-            where zs.zdravotnicke_zarizeni_kod in (
-                select min(zs1.zdravotnicke_zarizeni_kod) 
-                from zdravotnicke_stredisko zs1 
-                group by zs1.nrpzs_kod
-            ) 
             group by nrpzs_kod
         ) zs on (ol.zarizeni_kod=zs.nrpzs_kod)
 	    where zarizeni_kod not in (select nrpzs_kod from ockovaci_mista) and kraj_nuts_kod='{}'
