8e1f32e8379a0e8804edb87821ce6341f3b2da0e
Jan Staněk <jan@jstanek.cz>
2021-05-05 21:34:49 +0200

    population table update

---
+++ migrations/versions/25ebc99f8107_.py
@@ -0,0 +1,28 @@
+"""empty message
+
+Revision ID: 25ebc99f8107
+Revises: 12811ace3d10
+Create Date: 2021-05-05 21:24:51.713714
+
+"""
+import pandas as pd
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '25ebc99f8107'
+down_revision = '12811ace3d10'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    connection = op.get_bind()
+    df = pd.read_csv('data/populace.csv', delimiter=';')
+    df = pd.melt(df, id_vars=['vek'], var_name='orp_kod', value_name='pocet')
+    df.to_sql('populace', connection, if_exists='replace', index=False)
+
+
+def downgrade():
+    pass
