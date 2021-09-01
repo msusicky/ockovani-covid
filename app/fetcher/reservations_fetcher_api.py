1914e70eea53d382ccbc5b183cae6ef37141df07
Marek Susicky <marek@susicky.net>
2021-09-01 22:43:19 +0200

    Fixed fetcher

--- app/fetcher/reservations_fetcher_api.py
+++ app/fetcher/reservations_fetcher_api.py
@@ -1,5 +1,6 @@
 import os
-from datetime import date
+from datetime import date, datetime
+from typing import Optional
 
 import requests
 from dateutil.relativedelta import relativedelta
@@ -15,33 +16,42 @@ class ReservationsFetcherApi(Fetcher):
     Class for updating reservations table.
     """
 
-    API_URL = 'https://dev.reservatic.com/api/nakit/v2/'
+    API_URL = 'https://api.reservatic.com/api/nakit/v2/'
     VACCINE_SERVICES = 'vaccine_services/stats/'
 
     def __init__(self):
         super().__init__(OckovaniRezervace.__tablename__, self.API_URL + self.VACCINE_SERVICES, check_date=False)
 
+    def get_modified_date(self) -> Optional[datetime]:
+        return datetime.today()
+
     def fetch(self, import_id: int) -> None:
         token = os.environ.get('ODL_RESERVATIC_API')
+        if token is None:
+            raise ValueError("ODL_RESERVATIC_API variable is empty")
 
         ocms = db.session.query(OckovaciMisto) \
             .order_by(OckovaciMisto.id) \
             .all()
 
         # create the right date interval
-        week_before = date.now() + relativedelta(days=-7)
+        week_before = datetime.now() + relativedelta(days=-7)
         date_from = week_before.strftime('%Y-%m-%d')
-        month_after = date.now() + relativedelta(days=31)
+        month_after = datetime.now() + relativedelta(days=31)
         date_to = month_after.strftime('%Y-%m-%d')
 
         for ocm in ocms:
             '''iterate through all centers'''
-            # center='4c050cd4-6edf-4b4b-8034-8b731978b216'
             center = ocm.id
 
-            self._url = self._url + '{}?date_from={}&date_to={}'.format(center, week_before, month_after)
-            r = requests.get(self._url, headers={'Authorization': 'api_token ' + token}, verify=False)
-            df = DataFrame(r.json()['results'])
+            request_url = self._url + '{}?date_from={}&date_to={}'.format(center, date_from, date_to)
+            r = requests.get(request_url, headers={'api_token': '{}'.format(token)}, verify=False)
+
+            if r.status_code == 404:
+                # Place not found
+                continue
+
+            df = DataFrame(r.json())
 
             '''Iterate through the result'''
             '''Create rows'''
@@ -57,11 +67,11 @@ class ReservationsFetcherApi(Fetcher):
                 }
                 '''
                 db.session.add(OckovaniRezervace(
-                    datum=date,
+                    datum=row['date'],
                     ockovaci_misto_id=center,
-                    volna_kapacita=available_slots - reservations_count,
-                    maximalni_kapacita=available_slots,
-                    kalendar_ockovani=upper(vaccine_round),
-                    import_id= ?
+                    volna_kapacita=row['available_slots'] - row['reservations_count'],
+                    maximalni_kapacita=row['available_slots'],
+                    kalendar_ockovani=row['vaccine_round'].upper(),
+                    import_id=import_id
                 ))
         db.session.commit()
