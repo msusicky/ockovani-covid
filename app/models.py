620111bb1c02d895c82def1be5f19b1df58a69e3
Jan Staněk <jan@jstanek.cz>
2021-09-22 21:16:32 +0200

    migration fix

--- app/models.py
+++ app/models.py
@@ -817,6 +817,7 @@ class PrakticiKapacity(db.Model):
     kontakt_tel = Column(Unicode)
     poznamka = Column(Unicode)
     deti = Column(Boolean, nullable=False)
+    expirace = Column(Date)
 
     def __repr__(self):
         return f"<PrakticiKapacity(zdravotnicke_zarizeni_kod='{self.zdravotnicke_zarizeni_kod}')>"
