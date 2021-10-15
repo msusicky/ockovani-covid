8d319775de22e8e03fd78019c4c08e0eaac9830f
Jan Staněk <jan@jstanek.cz>
2021-10-16 00:12:54 +0200

    new fetchers

---
+++ app/fetcher/deaths_vaccinated_fetcher.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+from app import db
+from app.fetcher.fetcher import Fetcher
+from app.models import UmrtiOckovani
+
+
+class DeathsVaccinatedFetcher(Fetcher):
+    """
+    Class for updating deaths table.
+    """
+
+    DEATHS_CSV = 'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-umrti.csv'
+
+    def __init__(self):
+        super().__init__(UmrtiOckovani.__tablename__, self.DEATHS_CSV)
+
+    def fetch(self, import_id: int) -> None:
+        df = pd.read_csv(self._url)
+
+        df = df[['datum', 'zemreli_celkem', 'zemreli_bez_ockovani', 'zemreli_bez_ockovani_vek_prumer',
+                 'zemreli_nedokoncene_ockovani', 'zemreli_nedokoncene_ockovani_vek_prumer',
+                 'zemreli_dokoncene_ockovani', 'zemreli_dokoncene_ockovani_vek_prumer',
+                 'zemreli_posilujici_davka', 'zemreli_posilujici_davka_vek_prumer']]
+
+        df = df.rename(columns={'zemreli_celkem': 'celkem',
+                                'zemreli_bez_ockovani': 'bez_ockovani',
+                                'zemreli_bez_ockovani_vek_prumer': 'bez_ockovani_vek_prumer',
+                                'zemreli_nedokoncene_ockovani': 'nedokoncene_ockovani',
+                                'zemreli_nedokoncene_ockovani_vek_prumer': 'nedokoncene_ockovani_vek_prumer',
+                                'zemreli_dokoncene_ockovani': 'dokoncene_ockovani',
+                                'zemreli_dokoncene_ockovani_vek_prumer': 'dokoncene_ockovani_vek_prumer',
+                                'zemreli_posilujici_davka': 'posilujici_davka',
+                                'zemreli_posilujici_davka_vek_prumer': 'posilujici_davka_vek_prumer'})
+
+        self._truncate()
+
+        df.to_sql(self._table, db.engine, if_exists='append', index=False, method=Fetcher._psql_insert_copy)
