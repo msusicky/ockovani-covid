9d3e6048f0d72d1d8298022454f578acf9c0b814
Marek Susicky <marek@susicky.net>
2021-11-13 22:33:04 +0100

    Review change

--- app/fetcher/hospital_capacities_fetcher.py
+++ app/fetcher/hospital_capacities_fetcher.py
@@ -10,66 +10,17 @@ class HospitalCapacitiesFetcher(Fetcher):
     Class for updating capacities in hospitals.
     """
 
-    USED_CSV = 'https://dip.mzcr.cz/api/v1/kapacity-intenzivni-pece-zdravotnicke-zarizeni-10-2021.csv'
+    CAPACITIES_CSV = 'https://dip.mzcr.cz/api/v1/kapacity-intenzivni-pece-zdravotnicke-zarizeni-10-2021.csv'
 
     def __init__(self):
-        super().__init__(KapacityNemocnic.__tablename__, self.USED_CSV)
+        super().__init__(KapacityNemocnic.__tablename__, self.CAPACITIES_CSV)
 
     def fetch(self, import_id: int) -> None:
         df = pd.read_csv(self._url)
 
-        df['luzka_standard_kyslik_kapacita_volna_covid_pozitivni'] = df[
-            'luzka_standard_kyslik_kapacita_volna_covid_pozitivni'].fillna('0').astype('int')
-        df['luzka_standard_kyslik_kapacita_volna_covid_negativni'] = df[
-            'luzka_standard_kyslik_kapacita_volna_covid_negativni'].fillna('0').astype('int')
-        df['luzka_standard_kyslik_kapacita_celkem'] = df[
-            'luzka_standard_kyslik_kapacita_celkem'].fillna('0').astype('int')
-        df['luzka_hfno_cpap_kapacita_volna_covid_pozitivni'] = df[
-            'luzka_hfno_cpap_kapacita_volna_covid_pozitivni'].fillna('0').astype('int')
-        df['luzka_hfno_cpap_kapacita_volna_covid_negativni'] = df[
-            'luzka_hfno_cpap_kapacita_volna_covid_negativni'].fillna('0').astype('int')
-        df['luzka_hfno_cpap_kapacita_celkem'] = df[
-            'luzka_hfno_cpap_kapacita_celkem'].fillna('0').astype('int')
-        df['luzka_upv_niv_kapacita_volna_covid_pozitivni'] = df[
-            'luzka_upv_niv_kapacita_volna_covid_pozitivni'].fillna('0').astype('int')
-        df['luzka_upv_niv_kapacita_volna_covid_negativni'] = df[
-            'luzka_upv_niv_kapacita_volna_covid_negativni'].fillna('0').astype('int')
-        df['luzka_upv_niv_kapacita_celkem'] = df[
-            'luzka_upv_niv_kapacita_celkem'].fillna('0').astype('int')
-        df['inf_luzka_kyslik_kapacita_volna_covid_pozitivni'] = df[
-            'inf_luzka_kyslik_kapacita_volna_covid_pozitivni'].fillna('0').astype('int')
-        df['inf_luzka_kyslik_kapacita_volna_covid_negativni'] = df[
-            'inf_luzka_kyslik_kapacita_volna_covid_negativni'].fillna('0').astype('int')
-        df['inf_luzka_kyslik_kapacita_celkem'] = df[
-            'inf_luzka_kyslik_kapacita_celkem'].fillna('0').astype('int')
-        df['inf_luzka_hfno_kapacita_volna_covid_pozitivni'] = df[
-            'inf_luzka_hfno_kapacita_volna_covid_pozitivni'].fillna('0').astype('int')
-        df['inf_luzka_hfno_kapacita_volna_covid_negativni'] = df[
-            'inf_luzka_hfno_kapacita_volna_covid_negativni'].fillna('0').astype('int')
-        df['inf_luzka_hfno_kapacita_celkem'] = df[
-            'inf_luzka_hfno_kapacita_celkem'].fillna('0').astype('int')
-        df['inf_luzka_upv_kapacita_volna_covid_pozitivni'] = df[
-            'inf_luzka_upv_kapacita_volna_covid_pozitivni'].fillna('0').astype('int')
-        df['inf_luzka_upv_kapacita_volna_covid_negativni'] = df[
-            'inf_luzka_upv_kapacita_volna_covid_negativni'].fillna('0').astype('int')
-        df['inf_luzka_upv_kapacita_celkem'] = df[
-            'inf_luzka_upv_kapacita_celkem'].fillna('0').astype('int')
-        df['ventilatory_operacni_sal_kapacita_volna'] = df[
-            'ventilatory_operacni_sal_kapacita_volna'].fillna('0').astype('int')
-        df['ventilatory_operacni_sal_kapacita_celkem'] = df[
-            'ventilatory_operacni_sal_kapacita_celkem'].fillna('0').astype('int')
-        df['ecmo_kapacita_volna'] = df[
-            'ecmo_kapacita_volna'].fillna('0').astype('int')
-        df['ecmo_kapacita_celkem'] = df[
-            'ecmo_kapacita_celkem'].fillna('0').astype('int')
-        df['cvvhd_kapacita_volna'] = df[
-            'cvvhd_kapacita_volna'].fillna('0').astype('int')
-        df['cvvhd_kapacita_celkem'] = df[
-            'cvvhd_kapacita_celkem'].fillna('0').astype('int')
-        df['ventilatory_prenosne_kapacita_volna'] = df[
-            'ventilatory_prenosne_kapacita_volna'].fillna('0').astype('int')
-        df['ventilatory_prenosne_kapacita_celkem'] = df[
-            'ventilatory_prenosne_kapacita_celkem'].fillna('0').astype('int')
+        for col in df.columns:
+            if col not in ['datum', 'zz_kod', 'zz_nazev', 'kraj_nuts_kod', 'kraj_nazev']:
+                df[col] = df[col].fillna('0').astype('int')
 
         df = df.drop('kraj_nazev', axis=1)
 
