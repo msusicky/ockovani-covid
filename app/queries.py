f78d040223b5cf2e13e69ff6160da48069de75b9
Jan Staněk <jan@jstanek.cz>
2021-11-16 23:26:40 +0100

    comparison table

--- app/queries.py
+++ app/queries.py
@@ -922,7 +922,7 @@ def count_vaccinated_unvaccinated_comparison_age():
             df_norm[d + '_' + g + '_norm'] = ((100000 * df_norm[d + '_' + g]) / df_norm['populace_' + g]) \
                 .replace({np.nan: 0})
 
-        df_norm[d + '_ratio'] = (df_norm[d + '_bez_norm'] / df_norm[d + '_plne_norm']).replace({np.nan: None})
+        df_norm[d + '_ratio'] = (df_norm[d + '_bez_norm'] / df_norm[d + '_plne_norm']).replace({np.inf: None})
 
     for g in groups:
         df_norm['populace_' + g + '_zastoupeni'] = df_norm['populace_' + g] / df_norm['populace']
