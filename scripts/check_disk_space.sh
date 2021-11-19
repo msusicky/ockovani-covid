c0001c9b0878b6ae08d96f6b2f8b9d37a6e7a98d
Jan Staněk <jan@jstanek.cz>
2021-11-20 00:08:03 +0100

    workflows update

--- scripts/check_disk_space.sh
+++ scripts/check_disk_space.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-LIMIT=75
+LIMIT=70
 used_str=$(df -Ph . | tail -1 | awk '{print $5}')
 used_num=$(echo "$used_str" | tr -d "%")
 if [ $used_num -gt $LIMIT ]; then
   echo "Disk usage is over $LIMIT% ($used_str)!"
   exit 1
+else
+  echo "Disk usage is below $LIMIT% ($used_str)."
+  exit 0
 fi
