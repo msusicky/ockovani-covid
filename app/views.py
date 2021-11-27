a645c4f164ec1435c87ae1c708d3006b74a70a0f
Jan Staněk <jan@jstanek.cz>
2021-11-27 20:44:30 +0100

    stats split, cleanup, fixes

--- app/views.py
+++ app/views.py
@@ -140,322 +140,84 @@ def statistiky():
         .filter(CrMetriky.datum == get_import_date()) \
         .one_or_none()
 
-    end_date = queries.count_end_date_vaccinated()
+    eligible_count = queries.count_eligible()
 
-    end_date_supplies = queries.count_end_date_supplies()
+    # interest = queries.count_interest()
 
-    end_date_interested = queries.count_end_date_interested()
+    infected_orp_graph_data = queries.get_infected_orp_graph_data()
 
-    interest = queries.count_interest()
+    tests_orp_graph_data = queries.get_tests_orp_graph_data()
 
-    vaccines = queries.count_vaccines_cr()
+    hospitalized_orp_graph_data = queries.get_hospitalized_orp_graph_data()
 
-    vaccinated = queries.count_vaccinated()
-
-    supplies = queries.count_supplies()
-
-    end_date_category = queries.count_end_date_category()
-
-    vaccinated_category = queries.count_vaccinated_category()
-
-    reservations_category = queries.count_reservations_category()
-
-    vaccinated_week = queries.count_vaccinated_week()
-
-    top_centers = queries.count_top_centers()
+    vaccinated_orp_graph_data = queries.get_vaccinated_orp_graph_data()
 
     vaccinated_unvaccinated_comparison_age = queries.count_vaccinated_unvaccinated_comparison_age()
-
     vaccinated_unvaccinated_comparison_age_graph_data = vaccinated_unvaccinated_comparison_age[vaccinated_unvaccinated_comparison_age['vekova_skupina'] != '0-11']
 
-    # Source data for graph of received vaccines of the manufacturers
-    received_vaccine_graph_data = queries.get_received_vaccine_graph_data()
-
-    # Source data for graph of used vaccines based on the manufacturers
-    used_vaccine_graph_data = queries.get_used_vaccine_graph_data()
-
-    # Source data for graph of people in queue for the whole republic
-    queue_graph_data = queries.get_queue_graph_data()
-
     vaccination_total_graph_data = queries.get_vaccination_total_graph_data()
-
-    registrations_graph_data = queries.get_registrations_graph_data()
 
     infected_graph_data = queries.get_infected_graph_data()
 
     deaths_graph_data = queries.get_deaths_graph_data()
 
-    pocet_opravneni = queries.count_eligible()
-
-    infected_orp_graph_data = queries.get_infected_orp_graph_data()
-
-    vaccinated_orp_graph_data = queries.get_vaccinated_orp_graph_data()
-
-    hospitalized_orp_graph_data = queries.get_hospitalized_orp_graph_data()
-
-    tests_orp_graph_data = queries.get_tests_orp_graph_data()
-
     return render_template('statistiky.html', last_update=_last_import_modified(), now=_now(), metriky=metriky,
-                           end_date=end_date, end_date_supplies=end_date_supplies,
-                           end_date_interested=end_date_interested, interest=interest, vaccines=vaccines,
-                           vaccinated=vaccinated, vaccinated_category=vaccinated_category,
-                           reservations_category=reservations_category, supplies=supplies,
-                           end_date_category=end_date_category, vaccinated_week=vaccinated_week,
-                           top_centers=top_centers,
-                           vaccinated_unvaccinated_comparison_age=vaccinated_unvaccinated_comparison_age,
-                           received_vaccine_graph_data=received_vaccine_graph_data,
-                           used_vaccine_graph_data=used_vaccine_graph_data, queue_graph_data=queue_graph_data,
-                           vaccination_total_graph_data=vaccination_total_graph_data,
-                           registrations_graph_data=registrations_graph_data,
-                           infected_graph_data=infected_graph_data, deaths_graph_data=deaths_graph_data,
-                           pocet_opravneni=pocet_opravneni, infected_orp_graph_data=infected_orp_graph_data,
-                           vaccinated_orp_graph_data=vaccinated_orp_graph_data,
-                           hospitalized_orp_graph_data=hospitalized_orp_graph_data,
+                           eligible_count=eligible_count, infected_orp_graph_data=infected_orp_graph_data,
                            tests_orp_graph_data=tests_orp_graph_data,
-                           vaccinated_unvaccinated_comparison_age_graph_data=vaccinated_unvaccinated_comparison_age_graph_data)
+                           hospitalized_orp_graph_data=hospitalized_orp_graph_data,
+                           vaccinated_orp_graph_data=vaccinated_orp_graph_data,
+                           vaccinated_unvaccinated_comparison_age_graph_data=vaccinated_unvaccinated_comparison_age_graph_data,
+                           vaccination_total_graph_data=vaccination_total_graph_data,
+                           infected_graph_data=infected_graph_data, deaths_graph_data=deaths_graph_data)
 
 
 @bp.route("/statistiky_ockovani")
 def statistiky_ockovani():
-    metriky = db.session.query(CrMetriky) \
-        .filter(CrMetriky.datum == get_import_date()) \
-        .one_or_none()
-
-    end_date = queries.count_end_date_vaccinated()
-
-    end_date_supplies = queries.count_end_date_supplies()
-
-    end_date_interested = queries.count_end_date_interested()
-
-    interest = queries.count_interest()
-
-    vaccines = queries.count_vaccines_cr()
-
     vaccinated = queries.count_vaccinated()
 
-    supplies = queries.count_supplies()
+    vaccination_total_graph_data = queries.get_vaccination_total_graph_data()
 
-    end_date_category = queries.count_end_date_category()
+    reservations_category = queries.count_reservations_category()
 
     vaccinated_category = queries.count_vaccinated_category()
 
-    reservations_category = queries.count_reservations_category()
+    queue_graph_data = queries.get_queue_graph_data()
+
+    registrations_graph_data = queries.get_registrations_graph_data()
 
     vaccinated_week = queries.count_vaccinated_week()
 
     top_centers = queries.count_top_centers()
 
-    vaccinated_unvaccinated_comparison_age = queries.count_vaccinated_unvaccinated_comparison_age()
-
-    vaccinated_unvaccinated_comparison_age_graph_data = vaccinated_unvaccinated_comparison_age[vaccinated_unvaccinated_comparison_age['vekova_skupina'] != '0-11']
-
-    # Source data for graph of received vaccines of the manufacturers
-    received_vaccine_graph_data = queries.get_received_vaccine_graph_data()
-
-    # Source data for graph of used vaccines based on the manufacturers
     used_vaccine_graph_data = queries.get_used_vaccine_graph_data()
 
-    # Source data for graph of people in queue for the whole republic
-    queue_graph_data = queries.get_queue_graph_data()
+    received_vaccine_graph_data = queries.get_received_vaccine_graph_data()
 
-    vaccination_total_graph_data = queries.get_vaccination_total_graph_data()
-
-    registrations_graph_data = queries.get_registrations_graph_data()
-
-    infected_graph_data = queries.get_infected_graph_data()
-
-    deaths_graph_data = queries.get_deaths_graph_data()
-
-    pocet_opravneni = queries.count_eligible()
-
-    infected_orp_graph_data = queries.get_infected_orp_graph_data()
-
-    vaccinated_orp_graph_data = queries.get_vaccinated_orp_graph_data()
-
-    hospitalized_orp_graph_data = queries.get_hospitalized_orp_graph_data()
-
-    tests_orp_graph_data = queries.get_tests_orp_graph_data()
-
-    return render_template('statistiky_ockovani.html', last_update=_last_import_modified(), now=_now(), metriky=metriky,
-                           end_date=end_date, end_date_supplies=end_date_supplies,
-                           end_date_interested=end_date_interested, interest=interest, vaccines=vaccines,
-                           vaccinated=vaccinated, vaccinated_category=vaccinated_category,
-                           reservations_category=reservations_category, supplies=supplies,
-                           end_date_category=end_date_category, vaccinated_week=vaccinated_week,
-                           top_centers=top_centers,
-                           vaccinated_unvaccinated_comparison_age=vaccinated_unvaccinated_comparison_age,
-                           received_vaccine_graph_data=received_vaccine_graph_data,
-                           used_vaccine_graph_data=used_vaccine_graph_data, queue_graph_data=queue_graph_data,
-                           vaccination_total_graph_data=vaccination_total_graph_data,
-                           registrations_graph_data=registrations_graph_data,
-                           infected_graph_data=infected_graph_data, deaths_graph_data=deaths_graph_data,
-                           pocet_opravneni=pocet_opravneni, infected_orp_graph_data=infected_orp_graph_data,
-                           vaccinated_orp_graph_data=vaccinated_orp_graph_data,
-                           hospitalized_orp_graph_data=hospitalized_orp_graph_data,
-                           tests_orp_graph_data=tests_orp_graph_data,
-                           vaccinated_unvaccinated_comparison_age_graph_data=vaccinated_unvaccinated_comparison_age_graph_data)
+    return render_template('statistiky_ockovani.html', last_update=_last_import_modified(), now=_now(),
+                           vaccinated=vaccinated, vaccination_total_graph_data=vaccination_total_graph_data,
+                           reservations_category=reservations_category, vaccinated_category=vaccinated_category,
+                           queue_graph_data=queue_graph_data, registrations_graph_data=registrations_graph_data,
+                           vaccinated_week=vaccinated_week, top_centers=top_centers,
+                           used_vaccine_graph_data=used_vaccine_graph_data,
+                           received_vaccine_graph_data=received_vaccine_graph_data)
 
 
 @bp.route("/statistiky_srovnani")
 def statistiky_srovnani():
-    metriky = db.session.query(CrMetriky) \
-        .filter(CrMetriky.datum == get_import_date()) \
-        .one_or_none()
-
-    end_date = queries.count_end_date_vaccinated()
-
-    end_date_supplies = queries.count_end_date_supplies()
-
-    end_date_interested = queries.count_end_date_interested()
-
-    interest = queries.count_interest()
-
-    vaccines = queries.count_vaccines_cr()
-
-    vaccinated = queries.count_vaccinated()
-
-    supplies = queries.count_supplies()
-
-    end_date_category = queries.count_end_date_category()
-
-    vaccinated_category = queries.count_vaccinated_category()
-
-    reservations_category = queries.count_reservations_category()
-
-    vaccinated_week = queries.count_vaccinated_week()
-
-    top_centers = queries.count_top_centers()
-
     vaccinated_unvaccinated_comparison_age = queries.count_vaccinated_unvaccinated_comparison_age()
 
     vaccinated_unvaccinated_comparison_age_graph_data = vaccinated_unvaccinated_comparison_age[vaccinated_unvaccinated_comparison_age['vekova_skupina'] != '0-11']
 
-    # Source data for graph of received vaccines of the manufacturers
-    received_vaccine_graph_data = queries.get_received_vaccine_graph_data()
-
-    # Source data for graph of used vaccines based on the manufacturers
-    used_vaccine_graph_data = queries.get_used_vaccine_graph_data()
-
-    # Source data for graph of people in queue for the whole republic
-    queue_graph_data = queries.get_queue_graph_data()
-
-    vaccination_total_graph_data = queries.get_vaccination_total_graph_data()
-
-    registrations_graph_data = queries.get_registrations_graph_data()
-
-    infected_graph_data = queries.get_infected_graph_data()
-
-    deaths_graph_data = queries.get_deaths_graph_data()
-
-    pocet_opravneni = queries.count_eligible()
-
-    infected_orp_graph_data = queries.get_infected_orp_graph_data()
-
-    vaccinated_orp_graph_data = queries.get_vaccinated_orp_graph_data()
-
-    hospitalized_orp_graph_data = queries.get_hospitalized_orp_graph_data()
-
-    tests_orp_graph_data = queries.get_tests_orp_graph_data()
-
-    return render_template('statistiky_srovnani.html', last_update=_last_import_modified(), now=_now(), metriky=metriky,
-                           end_date=end_date, end_date_supplies=end_date_supplies,
-                           end_date_interested=end_date_interested, interest=interest, vaccines=vaccines,
-                           vaccinated=vaccinated, vaccinated_category=vaccinated_category,
-                           reservations_category=reservations_category, supplies=supplies,
-                           end_date_category=end_date_category, vaccinated_week=vaccinated_week,
-                           top_centers=top_centers,
+    return render_template('statistiky_srovnani.html', last_update=_last_import_modified(), now=_now(),
                            vaccinated_unvaccinated_comparison_age=vaccinated_unvaccinated_comparison_age,
-                           received_vaccine_graph_data=received_vaccine_graph_data,
-                           used_vaccine_graph_data=used_vaccine_graph_data, queue_graph_data=queue_graph_data,
-                           vaccination_total_graph_data=vaccination_total_graph_data,
-                           registrations_graph_data=registrations_graph_data,
-                           infected_graph_data=infected_graph_data, deaths_graph_data=deaths_graph_data,
-                           pocet_opravneni=pocet_opravneni, infected_orp_graph_data=infected_orp_graph_data,
-                           vaccinated_orp_graph_data=vaccinated_orp_graph_data,
-                           hospitalized_orp_graph_data=hospitalized_orp_graph_data,
-                           tests_orp_graph_data=tests_orp_graph_data,
                            vaccinated_unvaccinated_comparison_age_graph_data=vaccinated_unvaccinated_comparison_age_graph_data)
 
 
 @bp.route("/statistiky_nemocnice")
 def statistiky_nemocnice():
-    metriky = db.session.query(CrMetriky) \
-        .filter(CrMetriky.datum == get_import_date()) \
-        .one_or_none()
 
-    end_date = queries.count_end_date_vaccinated()
 
-    end_date_supplies = queries.count_end_date_supplies()
-
-    end_date_interested = queries.count_end_date_interested()
-
-    interest = queries.count_interest()
-
-    vaccines = queries.count_vaccines_cr()
-
-    vaccinated = queries.count_vaccinated()
-
-    supplies = queries.count_supplies()
-
-    end_date_category = queries.count_end_date_category()
-
-    vaccinated_category = queries.count_vaccinated_category()
-
-    reservations_category = queries.count_reservations_category()
-
-    vaccinated_week = queries.count_vaccinated_week()
-
-    top_centers = queries.count_top_centers()
-
-    vaccinated_unvaccinated_comparison_age = queries.count_vaccinated_unvaccinated_comparison_age()
-
-    vaccinated_unvaccinated_comparison_age_graph_data = vaccinated_unvaccinated_comparison_age[vaccinated_unvaccinated_comparison_age['vekova_skupina'] != '0-11']
-
-    # Source data for graph of received vaccines of the manufacturers
-    received_vaccine_graph_data = queries.get_received_vaccine_graph_data()
-
-    # Source data for graph of used vaccines based on the manufacturers
-    used_vaccine_graph_data = queries.get_used_vaccine_graph_data()
-
-    # Source data for graph of people in queue for the whole republic
-    queue_graph_data = queries.get_queue_graph_data()
-
-    vaccination_total_graph_data = queries.get_vaccination_total_graph_data()
-
-    registrations_graph_data = queries.get_registrations_graph_data()
-
-    infected_graph_data = queries.get_infected_graph_data()
-
-    deaths_graph_data = queries.get_deaths_graph_data()
-
-    pocet_opravneni = queries.count_eligible()
-
-    infected_orp_graph_data = queries.get_infected_orp_graph_data()
-
-    vaccinated_orp_graph_data = queries.get_vaccinated_orp_graph_data()
-
-    hospitalized_orp_graph_data = queries.get_hospitalized_orp_graph_data()
-
-    tests_orp_graph_data = queries.get_tests_orp_graph_data()
-
-    return render_template('statistiky_nemocnice.html', last_update=_last_import_modified(), now=_now(), metriky=metriky,
-                           end_date=end_date, end_date_supplies=end_date_supplies,
-                           end_date_interested=end_date_interested, interest=interest, vaccines=vaccines,
-                           vaccinated=vaccinated, vaccinated_category=vaccinated_category,
-                           reservations_category=reservations_category, supplies=supplies,
-                           end_date_category=end_date_category, vaccinated_week=vaccinated_week,
-                           top_centers=top_centers,
-                           vaccinated_unvaccinated_comparison_age=vaccinated_unvaccinated_comparison_age,
-                           received_vaccine_graph_data=received_vaccine_graph_data,
-                           used_vaccine_graph_data=used_vaccine_graph_data, queue_graph_data=queue_graph_data,
-                           vaccination_total_graph_data=vaccination_total_graph_data,
-                           registrations_graph_data=registrations_graph_data,
-                           infected_graph_data=infected_graph_data, deaths_graph_data=deaths_graph_data,
-                           pocet_opravneni=pocet_opravneni, infected_orp_graph_data=infected_orp_graph_data,
-                           vaccinated_orp_graph_data=vaccinated_orp_graph_data,
-                           hospitalized_orp_graph_data=hospitalized_orp_graph_data,
-                           tests_orp_graph_data=tests_orp_graph_data,
-                           vaccinated_unvaccinated_comparison_age_graph_data=vaccinated_unvaccinated_comparison_age_graph_data)
+    return render_template('statistiky_nemocnice.html', last_update=_last_import_modified(), now=_now())
 
 
 @bp.route("/napoveda")
