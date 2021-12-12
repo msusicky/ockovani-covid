033910d9022fac9efb893cf09542ef72b918a94f
Jan Staněk <jan@jstanek.cz>
2021-12-12 20:56:08 +0100

    Third doses in tweet

--- app/twitter_bot.py
+++ app/twitter_bot.py
@@ -10,21 +10,27 @@ from app.models import CrMetriky
 class TwitterBot():
     def __init__(self):
         stats = db.session.query(CrMetriky.ockovani_pocet_plne, CrMetriky.ockovani_pocet_plne_zmena_den,
+                                 CrMetriky.ockovani_pocet_3, CrMetriky.ockovani_pocet_3_zmena_den,
                                  CrMetriky.pocet_obyvatel_celkem, CrMetriky.registrace_pred_zavorou,
                                  CrMetriky.registrace_prumer_cekani) \
             .filter(CrMetriky.datum == get_import_date()) \
             .one()
 
-        self._vaccinated = stats.ockovani_pocet_plne
-        self._vaccinated_diff = stats.ockovani_pocet_plne_zmena_den
-        self._vaccinated_ratio = (1.0 * stats.ockovani_pocet_plne) / stats.pocet_obyvatel_celkem
+        self._vaccinated_2 = stats.ockovani_pocet_plne
+        self._vaccinated_2_diff = stats.ockovani_pocet_plne_zmena_den
+        self._vaccinated_2_ratio = stats.ockovani_pocet_plne / stats.pocet_obyvatel_celkem
+
+        self._vaccinated_3 = stats.ockovani_pocet_3
+        self._vaccinated_3_diff = stats.ockovani_pocet_3_zmena_den
+        self._vaccinated_3_ratio = stats.ockovani_pocet_3 / stats.pocet_obyvatel_celkem
+
         self._waiting = stats.registrace_pred_zavorou
         self._average_reservation_waiting = stats.registrace_prumer_cekani
         # self._end_date = queries.count_end_date_vaccinated()
         # self._end_date_supplies = queries.count_end_date_supplies()
         # self._end_date_interested = queries.couht_end_date_interested()
         # self._interest_all = queries.count_interest()[0]
-        self._gp_vaccines = queries.couht_gp_vaccines()
+        # self._gp_vaccines = queries.couht_gp_vaccines()
 
     def post_tweet(self):
         text = self._generate_tweet()
@@ -39,13 +45,14 @@ class TwitterBot():
         return True
 
     def _generate_tweet(self):
-        return f"{self._generate_progressbar()} plně očkováno ({filters.format_number(self._vaccinated)} celkem, {filters.format_number(self._vaccinated_diff)} od včera). " \
-               f"Na termín pro 1. nebo 3. dávku čeká před závorou {filters.format_number(self._waiting)} zájemců, průměrná doba čekání je {filters.format_number(self._average_reservation_waiting)} dní. " \
-               f"Praktici nabízejí {filters.format_number(self._gp_vaccines)} dávek. https://ockovani.opendatalab.cz"
+        return f"{self._generate_progressbar(self._vaccinated_2_ratio)} 💉💉 ({filters.format_number(self._vaccinated_2)} celkem, {filters.format_number(self._vaccinated_2_diff)} od včera).\n" \
+               f"{self._generate_progressbar(self._vaccinated_3_ratio)} 💉💉💉 ({filters.format_number(self._vaccinated_3)} celkem, {filters.format_number(self._vaccinated_3_diff)} od včera).\n" \
+               f"Na termín čeká před závorou {filters.format_number(self._waiting)} zájemců, průměrné čekání je {self._days(self._average_reservation_waiting)}. " \
+               f"https://ockovani.opendatalab.cz"
 
-    def _generate_progressbar(self):
-        vaccinated_percent = self._vaccinated_ratio * 100
-        vaccinated_progress = self._vaccinated_ratio * 20
+    def _generate_progressbar(self, ratio):
+        vaccinated_percent = ratio * 100
+        vaccinated_progress = ratio * 20
 
         progressbar = ''
 
@@ -56,6 +63,14 @@ class TwitterBot():
                 progressbar += '░'
 
         return progressbar + ' ' + filters.format_decimal(vaccinated_percent, 1) + ' %'
+
+    def _days(self, days):
+        if days == 1:
+            return filters.format_number(days) + " den"
+        elif days >= 2 and days <= 4:
+            return filters.format_number(days) + " dny"
+        else:
+            return filters.format_number(days) + " dní"
 
     def _post_tweet(self, text):
         api = twitter.Api(consumer_key=app.config['TWITTER_CONSUMER_KEY'],
