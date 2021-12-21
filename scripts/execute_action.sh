9d85b37dd92ca223a48071fbc7f348d67130984a
Jan Staněk <jan@jstanek.cz>
2021-12-21 23:08:25 +0100

    cron fixed

--- scripts/execute_action.sh
+++ scripts/execute_action.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -X POST 'https://api.github.com/repos/msusicky/ockovani-covid/actions/workflows/$1/dispatches' -H 'Authorization: token $GH_TOKEN' -H 'Accept: application/vnd.github.v3+json' -d '{ "ref": "main" }'
+source /etc/env/ockovani.env
+curl -X POST "https://api.github.com/repos/msusicky/ockovani-covid/actions/workflows/$1/dispatches" -H "Authorization: token $GH_TOKEN" -H "Accept: application/vnd.github.v3+json" -d '{ "ref": "main" }'
